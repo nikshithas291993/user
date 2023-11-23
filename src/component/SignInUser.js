7dec685a1a3d7fb2902dfbdb4cc6242f18ee5de8
Nikshitha Shetty <nikshitha.s@sonata-software.com>
2023-11-23 13:40:43 +0530

    Update on signin

--- src/component/SignInUser.js
+++ src/component/SignInUser.js
@@ -1,17 +1,22 @@
-import React, { useState } from "react";
+import React, { useState ,useContext } from "react";
 import SignInForm from "./SignInForm";
 import { Alert, Snackbar } from "@mui/material";
 import { signInApi } from "../../../container/src/api-helper/frontend/util";
 import { useNavigate } from "react-router-dom";
+//import SharedContext from "../../../container/src/context/Shared";
 const SignInUser = () =>{
+        //const sharedContext = useContext(SharedContext);
         const navigate = useNavigate();
         const [message, setMessage] = useState(null);
         const [showMessage, setShowMessage] = useState(null);
         const[open, setOpen] = useState(false);
         const getFormdata = (data) =>{
             signInApi(data).then((value) =>{
-                console.log(value)
+                const userData = {
+                    username: 'Nikshitha',
+                  };
                 if(!value.error){
+                    localStorage.setItem('userData', JSON.stringify(userData));
                     navigate("/");
                     setShowMessage(value.error)
                     setOpen(true)
@@ -21,8 +26,9 @@ const SignInUser = () =>{
                     setOpen(true)
                     setMessage(value.message)
                 }
+                window.dispatchEvent(new CustomEvent('userLoggedIn', { detail: userData }));
             })
-            .then(() => console.log("succeas"))
+            .then(() => console.log("success"))
             .catch((err) =>console.log(err));
 
         };
